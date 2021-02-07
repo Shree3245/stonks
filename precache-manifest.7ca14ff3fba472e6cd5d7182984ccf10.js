42ec27047cdb0978a94bc5b1fd821ab93ba24019
Shree3245 <Shreevenkr@gmail.com>
2021-02-07 10:06:15 +0530

    Updates

---
+++ precache-manifest.7ca14ff3fba472e6cd5d7182984ccf10.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7379b8a7841fc6f9ee11e319bc65e7d0",
+    "url": "/stonks/index.html"
+  },
+  {
+    "revision": "d512a76c04aa5f2a1719",
+    "url": "/stonks/static/css/main.62d25267.chunk.css"
+  },
+  {
+    "revision": "dbcf9b0a6825b0accdc3",
+    "url": "/stonks/static/js/2.a6761339.chunk.js"
+  },
+  {
+    "revision": "d8512e9b43b122a7f3c55b66071bf325",
+    "url": "/stonks/static/js/2.a6761339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d512a76c04aa5f2a1719",
+    "url": "/stonks/static/js/main.5eb8e8a8.chunk.js"
+  },
+  {
+    "revision": "2784667e535464a459ec",
+    "url": "/stonks/static/js/runtime-main.3310843e.js"
+  },
+  {
+    "revision": "ac3c1298def54faa25ba494490ed4f4c",
+    "url": "/stonks/static/media/NeutrifPro-Bold.ac3c1298.otf"
+  },
+  {
+    "revision": "76f0c14e70c41e5314700073e6919b00",
+    "url": "/stonks/static/media/NeutrifPro-Light.76f0c14e.otf"
+  },
+  {
+    "revision": "ea2339315a9d52cb3e7f51328fa10af0",
+    "url": "/stonks/static/media/NeutrifPro-Medium.ea233931.otf"
+  },
+  {
+    "revision": "52b0d8c3708d691c44d5156a9e5f2d6b",
+    "url": "/stonks/static/media/NeutrifPro-Regular.52b0d8c3.otf"
+  },
+  {
+    "revision": "d5e437924301262fcae652f8c010086f",
+    "url": "/stonks/static/media/chart-line-down.d5e43792.svg"
+  },
+  {
+    "revision": "3228d26428457a38a1f31063668593e2",
+    "url": "/stonks/static/media/chart-line.3228d264.svg"
+  },
+  {
+    "revision": "6db61f5f7bcc4b5e150439f3f745c6e8",
+    "url": "/stonks/static/media/chart-pie-alt.6db61f5f.svg"
+  },
+  {
+    "revision": "b79f7d72a4a12acad952bdef728d3053",
+    "url": "/stonks/static/media/chevron-down.b79f7d72.svg"
+  },
+  {
+    "revision": "c2d439afe62dfcd47b72275fa27597cb",
+    "url": "/stonks/static/media/coins.c2d439af.svg"
+  },
+  {
+    "revision": "33b4607528e079faff1e1506e9e5eee9",
+    "url": "/stonks/static/media/flask.33b46075.svg"
+  },
+  {
+    "revision": "1d7ac05c60b04c0be76d809a0a84faf4",
+    "url": "/stonks/static/media/globe.1d7ac05c.svg"
+  },
+  {
+    "revision": "acd3dbbf366fc5d3971de9fe7193d0b7",
+    "url": "/stonks/static/media/piggy-bank.acd3dbbf.svg"
+  },
+  {
+    "revision": "a78611684cf0658c2bccd0fbc4ffb4c4",
+    "url": "/stonks/static/media/star.a7861168.svg"
+  }
+]);
